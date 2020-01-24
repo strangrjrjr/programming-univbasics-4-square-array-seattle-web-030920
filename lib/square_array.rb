70d4da2778f8b37c9ef601d32195a781cce0f7cf
Roger Robertson Jr <rjrjr@pm.me>
2020-01-24 19:03:36 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  count = 0
+  sq_array = []
+  while count != array.length do
+    sq_array.push(array[count] ** 2)
+    count += 1
+  end
+  return sq_array
 end
